b19ce31409d23d90121ca6fbcd031562e11c3bd9
Nicholas J Humfrey <njh@aelius.com>
2012-12-27 17:12:28 +0000

    Changed bootstrap-response.css to be generated by LESS PHP too.

--- scripts/compile-less.php
+++ scripts/compile-less.php
@@ -1,15 +1,26 @@
 <?php
+//
+// Script to build custom Bootstrap CSS from LESS sources
+//
 
 $root = realpath(__DIR__ . "/..");
 require "$root/vendor/autoload.php";
 
+// Location of bootstrap source LESS files
+$bootstrap = "$root/vendor/twitter/bootstrap/twitter/bootstrap/less";
+
 $less = new lessc;
 $less->setImportDir(array(
     "$root/less",
-    "$root/vendor/twitter/bootstrap/twitter/bootstrap/less"
+    $bootstrap
 ));
 
 $less->compileFile(
-    "$root/less/easyrdf.less",
+    "$root/less/bootstrap.less",
     "$root/public/css/bootstrap.css"
+);
+
+$less->compileFile(
+    "$bootstrap/responsive.less",
+    "$root/public/css/bootstrap-responsive.css"
 );
